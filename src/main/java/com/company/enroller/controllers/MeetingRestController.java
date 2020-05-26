d961ae96851cedbdd063b774419d5b54e2b60caa
Krzysztof Duda <duda_krzysztof@o2.pl>
2020-05-26 21:12:00 +0200

    First endpoint

--- src/main/java/com/company/enroller/controllers/MeetingRestController.java
+++ src/main/java/com/company/enroller/controllers/MeetingRestController.java
@@ -6,6 +6,8 @@ import com.company.enroller.persistence.ParticipantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,8 +26,33 @@ public class MeetingRestController {
 
     @RequestMapping(value = "", method = RequestMethod.GET)
     public ResponseEntity<?> getMeetings() {
-
         Collection<Meeting> meetings = meetingService.getAll();
         return new ResponseEntity<Collection<Meeting>>(meetings, HttpStatus.OK);
     }
+    
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> getMeeting(@PathVariable("id") long id) {
+		Meeting meeting = meetingService.findById(id);
+		return new ResponseEntity<Meeting>(meeting, HttpStatus.OK); 
+	}
+
+	@RequestMapping(value = "", method = RequestMethod.POST)         
+	public ResponseEntity<?> registerMeeting(@RequestBody Meeting meeting) {                 
+		Meeting foundMeeting = meetingService.findById(meeting.getId());                 
+		if (foundMeeting != null) {                         
+			return new ResponseEntity("Unable to create. A meeting with Id " + meeting.getId() + " already exist.", HttpStatus.CONFLICT);
+			}                 
+	meetingService.add(meeting);                 
+	return new ResponseEntity<Meeting>(meeting, HttpStatus.CREATED);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)         
+	public ResponseEntity<?> delete(@PathVariable("id") long id) {                 
+		Meeting meeting = meetingService.findById(id);                 
+		if (meeting == null) {                         
+			return new ResponseEntity(HttpStatus.NOT_FOUND);
+		}
+	meetingService.delete(meeting);                 
+	return new ResponseEntity<Meeting>(meeting, HttpStatus.OK);
+	}
 }
--- src/main/java/com/company/enroller/model/Meeting.java
+++ src/main/java/com/company/enroller/model/Meeting.java
@@ -25,7 +25,9 @@ public class Meeting {
     private String date;
 
     @JsonIgnore
-    @ManyToMany(mappedBy = "meetings")
+    @ManyToMany(mappedBy = "meetings", cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+	@JoinTable(name = "meeting_participant", joinColumns = { @JoinColumn(name = "meeting_id") }, inverseJoinColumns = {
+	@JoinColumn(name = "participant_login") })
     Set<Participant> participants = new HashSet<>();
 
     public long getId() {
